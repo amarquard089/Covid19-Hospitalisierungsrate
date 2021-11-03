3036f21ba389b7709b8d60c7e2be96983275a9ca
Alexander <marquard82@googlemail.com>
2021-11-03 09:19:22 +0100

    added data (not complete yet)

---
+++ covid19_data_not_complete.R
@@ -0,0 +1,32 @@
+
+
+
+divi_tagesreport <- read.csv("https://www.divi.de/divi-intensivregister-tagesreport-archiv-csv/viewdocument/6071/divi-intensivregister-2021-11-02-12-15")
+
+divi_tagesreport$bundesland_fact <- as.factor(divi_tagesreport$bundesland)
+
+
+# Klinische Aspekte Excel
+## Bitte Tabelle anschauen ##
+
+Klinische_Aspekte <- read_excel("Klinische_Aspekte.xlsx", 
+                                sheet = "Klinische_Aspekte", skip = 2)
+
+Faelle_Hospitalisierung_Alter <- read_excel("Klinische_Aspekte.xlsx", 
+                                            sheet = "Fälle_Hospitalisierung_Alter", 
+                                            skip = 4)
+
+Alter_Median_Mittelwert <- read_excel("Klinische_Aspekte.xlsx", 
+                                      sheet = "Alter_Median_Mittelwert", 
+                                      col_types = c("date", "text", "numeric", 
+                                                    "numeric", "numeric", 
+                                                    "numeric", "skip", "skip", 
+                                                    "skip", "date", "text", 
+                                                    "numeric", "numeric", 
+                                                    "numeric", "numeric", 
+                                                    "numeric"), 
+                                      skip = 3)
+
+Sieben_Tage_Inzidenz_Hosp_Alter <- read_excel("Klinische_Aspekte.xlsx", 
+                                          sheet = "7-Tage-Inzidenz_Hosp_Alter", 
+                                          skip = 3)
